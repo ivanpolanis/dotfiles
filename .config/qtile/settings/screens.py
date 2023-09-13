06a0fcd05fa85053feb1c13cac8164581f4d8177
Ivan Polanis <polanisivan@gmail.com>
2023-09-13 20:20:33 -0300

    changing padding

--- .config/qtile/settings/screens.py
+++ .config/qtile/settings/screens.py
@@ -9,9 +9,9 @@ def status_bar(widgets):
     return bar.Bar(widgets,26, margin=[0, 0, 8, 0], border_width=[0, 0, 3, 0], border_color="#414868")
 
 
-screens = [Screen(top=status_bar(primary_widgets), bottom=bar.Gap(6),
-        left=bar.Gap(6),
-        right=bar.Gap(6),)]
+screens = [Screen(top=status_bar(primary_widgets), bottom=bar.Gap(4),
+        left=bar.Gap(4),
+        right=bar.Gap(4),)]
 
 xrandr = "xrandr | grep -w 'connected' | cut -d ' ' -f 2 | wc -l"
 
