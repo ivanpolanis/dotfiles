c51a42685fbbc5f3a8ec038eedac5f480e4d03e3
Ivan Polanis <polanisivan@gmail.com>
2023-09-13 22:16:35 -0300

    adding .local

---
+++ .local/bin/powermenu.sh
@@ -0,0 +1,45 @@
+#!/bin/env bash
+
+# Options for powermenu
+lock="    Lock"
+logout="󰗽    Logout"
+shutdown="󰐥    Shutdown"
+reboot="    Reboot"
+sleep="󰒲    Sleep"
+
+# Get answer from user via rofi
+selected_option=$(echo "$lock
+$logout
+$sleep
+$reboot
+$shutdown" | rofi -dmenu\
+                  -i\
+                  -p "Power"\
+                  -config "~/.config/rofi/powermenu.rasi"\
+                  -width "15"\
+                  -lines 5\
+                  -line-margin 3\
+                  -line-padding 10\
+                  -scrollbar-width "0" )
+
+# Do something based on selected option
+if [ "$selected_option" == "$lock" ]
+then
+    i3lock-fancy
+elif [ "$selected_option" == "$logout" ]
+then
+    loginctl terminate-user `whoami`
+elif [ "$selected_option" == "$shutdown" ]
+then
+    systemctl poweroff
+elif [ "$selected_option" == "$reboot" ]
+then
+    systemctl reboot
+elif [ "$selected_option" == "$sleep" ]
+then
+    amixer set Master mute
+    systemctl suspend
+else
+    echo "No match"
+fi
+
