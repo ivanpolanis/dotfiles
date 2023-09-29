829ab3d832b98e5acb47f5982849eefd58348d55
Ivan Polanis <polanisivan@gmail.com>
2023-09-29 09:55:13 -0300

    Removing Documents

--- Documents/sl_m_d/target/debug/deps/SL_M_D-745f8cce5a2618a2.d
+++
@@ -1,5 +0,0 @@
-/home/ivan/Documents/SL-M-D/target/debug/deps/SL_M_D-745f8cce5a2618a2.rmeta: src/main.rs
-
-/home/ivan/Documents/SL-M-D/target/debug/deps/SL_M_D-745f8cce5a2618a2.d: src/main.rs
-
-src/main.rs:
--- Documents/sl_m_d/target/debug/deps/sl_m_d-3aba42d17b0032b7.d
+++
@@ -1,5 +0,0 @@
-/home/ivan/Documents/sl_m_d/target/debug/deps/sl_m_d-3aba42d17b0032b7.rmeta: src/main.rs
-
-/home/ivan/Documents/sl_m_d/target/debug/deps/sl_m_d-3aba42d17b0032b7.d: src/main.rs
-
-src/main.rs:
