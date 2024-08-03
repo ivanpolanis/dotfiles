e912220163dcd1fc7cb2b01eed13147e49badfc2
Ivan Polanis <polanisivan@gmail.com>
2024-08-03 12:22:12 -0300

    trying new setup

---
+++ .local/bin/package_installer.py
@@ -0,0 +1,73 @@
+import os
+import subprocess
+import argparse
+
+def run_command(command):
+    try:
+        subprocess.run(command, check=True, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {command}")
+        print(e)
+
+def check_packages_exist(package_list,package_manager):
+    valid_packages = []
+    for package in package_list:
+        result = subprocess.run(f"{package_manager} -Sp {package} --noconfirm", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if result.returncode == 0:
+            valid_packages.append(package)
+        else:
+            print(f"Package {package} does not exist in repositories.")
+    return valid_packages
+
+def read_programs(file_path):
+    programs = {'pacman': [], 'aur': [], 'pip': [], 'git':[]}
+    with open(file_path,'r') as file:
+        for line in file:
+            if line.startswith('#') or not line.strip():
+                continue
+            tag, program = line.strip().split(',')
+            match tag:
+                case 'p':
+                    programs['pacman'].append(program)
+                case 'a':
+                    programs['aur'].append(program)
+                case 'i':
+                    programs['pip'].append(program)
+                case 'g':
+                    programs['git'].append(program)
+    return programs
+
+def install_packages(package_list, package_manager):
+    if package_list:
+        packages = ' '.join(package_list)
+        match package_manager:
+            case 'pacman':
+                run_command(f"sudo pacman --noconfirm --needed -S {packages}")
+            case 'pip':
+                run_command(f"pip install --break-system-packages {packages}")
+            case 'aur':
+                run_command(f"sudo -u {username} {aurhelper} -S --noconfirm {packages}")
+        
+def main():
+    global username, aurhelper
+
+    parser = argparse.ArgumentParser(description="Automate Archlinux configuration setup")
+    parser.add_argument("--username", required=True, help="Username for the installation")
+    parser.add_argument("--aurhelper", default="paru", help="AUR helper to use (default: paru)")
+    parser.add_argument("--progsfile", default="~/progs.csv", help="Path to the programs file")
+    args = parser.parse_args()
+
+    username = args.username
+    aurhelper = args.aurhelper
+    progsfile = os.path.expanduser(args.progsfile)
+    
+    packages = read_programs(progsfile)
+
+
+    packages['pacman'] = check_packages_exist(packages['pacman'],'pacman')
+    install_packages(packages['pacman'],'pacman')
+    install_packages(packages['aur'],'aur')
+    install_packages(packages['pip'],'pip')
+
+if __name__ == "__main__":
+    main()
--- autoinstall.sh
+++ autoinstall.sh
@@ -5,6 +5,7 @@
 ### VARIABLES ###
 
 aurhelper="paru"
+script_dir=$(dirname $(readlink -f "$0"))
 
 ### FUNCTIONS ###
 
@@ -18,7 +19,7 @@ usercheck() {
   #checks username
   echo -e "Enter usename:"
   read name
-  id "$name" > /dev/null 2> /dev/null || error "Invalid username!"
+  id "$name" >/dev/null 2>/dev/null || error "Invalid username!"
   mkdir -p "/home/$name/.local/src"
   export homedir="/home/$name"
   export repodir="/home/$name/.local/src"
@@ -36,17 +37,17 @@ install_aur() {
   sudo -u "$name" mkdir -p "$repodir/$1" #create directory
   sudo -u "$name" git -C "$repodir" clone --depth 1 --no-tags -q "https://aur.archlinux.org/$1.git" "$repodir/$1"
   cd "$repodir/$1" || return 1
-  sudo -u "$name" makepkg --noconfirm -si "$repodir/$1" > /dev/null 2>&1 || return 1
+  sudo -u "$name" makepkg --noconfirm -si "$repodir/$1" >/dev/null 2>&1 || return 1
 }
 
 installpkg() {
   #install packages wihtout confirming and avoid updating already installed packages
-  pacman --noconfirm --needed -S "$1" > /dev/null 2>&1 || echo -e "Error installing $1 (PACMAN)"
+  pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 || echo -e "Error installing $1 (PACMAN)"
 }
 
 aurinstall() {
   echo "$aurinstalled" | grep -q "^$1$" && return 1
-  sudo -u "$name" $aurhelper -S --noconfirm "$1" > /dev/null 2>&1 || echo "Failed installing $1 (AUR)"
+  sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1 || echo "Failed installing $1 (AUR)"
 }
 
 gitinstall() {
@@ -56,48 +57,12 @@ gitinstall() {
   progname="${progname%.git}"
   dir="$repodir/$progname"
   sudo -u "$name" git -C "$repodir" clone --depth 1 --single-branch \
-    --no-tags -q "$1" "$dir" \
-    || {
+    --no-tags -q "$1" "$dir" ||
+    {
       cd "$dir" || echo "Failed installing $1 (GIT)"
       sudo -u "$name" git pull --force origin master
     }
   cd "$dir" || return 1
-}
-
-pipinstall() {
-  #if pip is not already installed, it does
-  [ -x "$(command -v "pip")" ] || installpkg python-pip > /dev/null 2>&1
-  echo "$pipinstalled" | grep -q "^$1$" && return 1
-  pip install --break-system-packages $1 > /dev/null 2>&1 || echo "Failed installing $1 (PIP)"
-}
-
-installationloop() {
-  progsfile="$homedir/progs.csv"
-  #using a temp file to prevent editing the original programs file
-  ([ -f "$progsfile" ] && sed '/^#/d' "$progsfile" > /tmp/progs.csv) \
-    || error "Programs file not found"
-  aurinstalled="$(pacman -Qqm)"
-  gitinstalled="$(ls "$repodir")"
-  pipinstalled="$(pip list | awk '{print $1}' | tail -n +3)"
-  tmpfile="/tmp/progs.csv"
-  while IFS=, read -r tag program; do
-    echo "Installing $program"
-    case "$tag" in
-      "p") installpkg "$program" ;;
-      "g") gitinstall "$program" ;;
-      "a") aurinstall "$program" ;;
-      "i") pipinstall "$program" ;;
-    esac
-  done < "$tmpfile"
-}
-
-update_grub() {
-  echo "Do you want to update grub? (y/n)"
-  read option
-  [[ $option != 'y' ]] && return 0
-  [[ -d /tmp ]] || mkdir /tmp
-  git clone https://github.com/vinceliuice/grub2-themes.git /tmp/grub2-themes && cd /tmp/grub2-themes
-  sudo ./install.sh -t vimix
 }
 
 ### SCRIPT ###
@@ -108,7 +73,7 @@ usercheck
 
 echo "##### Installing all dependencies #####"
 
-for x in sudo zsh base-devel ca-certificates python-pip; do
+for x in sudo zsh base-devel ca-certificates python python-pip; do
   installpkg "$x"
 done
 
@@ -118,23 +83,25 @@ done
 # in a fakeroot environment, this is required for all builds with AUR.
 
 trap 'rm -f /etc/sudoers.d/larbs-temp' HUP INT QUIT TERM PWR EXIT # delete file if user exits
-echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/larbs-temp # allow wheel users(everyone) to run sudo without password
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/larbs-temp  # allow wheel users(everyone) to run sudo without password
 
 # Install aur helper manually
 echo "##### Installing AUR Helper #####"
 install_aur "${aurhelper}" || error "Failed to install AUR helper"
 
 # Main instalattion loop
+progsfile=$(echo $"script_dir"/progs.csv)
+[[ -f "$progsfile" ]] || error "Program file doesn't exist"
+python "/home/$name/.local/bin/package_installer.py" --username "$name" --aurhelper "$aurhelper" --progsfile "$progsfile"
 
-installationloop
+cd $script_dir"
+stow --adopt .
 
 # Make zsh the default shell for the user.
-chsh -s /bin/zsh "$name" > /dev/null 2>&1
+chsh -s /bin/zsh "$name" >/dev/null 2>&1
 sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
 
 #enable lightdm service
 systemctl enable lightdm
-
-update_grub
 
 echo -e "DONE! Now reboot your computer"
